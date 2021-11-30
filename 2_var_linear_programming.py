921bdeeaae0c99e6d9c3200bec94ffe153685748
Mannan-engprog <91082268+Mannan-engprog@users.noreply.github.com>
2021-11-30 11:29:33 +0530

    Add files via upload

---
+++ 2_var_linear_programming.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+row = ['RM of M1', 'RM of M2','Demand diff','daily demand']
+col = ['Exterior paint','interior paint','Available Qty']
+df = pd.DataFrame(index=row,columns=col)
+
